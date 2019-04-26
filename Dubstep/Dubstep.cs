5c169c5e91e1d3c44fd2b15a87b1bf5e5f86c2bc
Jimachine <jimmy_onion@hotmail.com>
2019-04-26 14:30:48 +0100

    string is decoding but index out of range exception thrown

---
+++ Dubstep/Dubstep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dubstep
+{
+    public class Dubstep
+    {
+        public static string SongDecoder(string input)
+        {
+            // if index 0 is W, is index 2 U and index 3 B?
+            string decodedString = input;
+            int test = input.Length;
+
+            while (decodedString.Contains("WUB"))
+            {
+                for (int c = 0; c < decodedString.Length; c++)
+                {
+                    if (decodedString[c] == 'W' & decodedString[c + 1] == 'U' & decodedString[c + 2] == 'B')
+                    {
+                        decodedString = decodedString.Remove(c, 3);
+                        c = 0;
+                    }
+                }
+            }
+            Console.WriteLine(decodedString);
+            return decodedString;
+        }
+    }
+}
